21fc502ee276ea4137aae03fed7fa7c115ed4630
geooooo <geo97it@gmail.com>
2018-04-28 12:43:59 +0300

    Lang: append lab 2

---
+++ lang/2/main.py
@@ -0,0 +1,142 @@
+import re
+
+
+class Stemmer:
+
+    _vowel = "[аеиоуыэюя]"
+    _non_vowel = "[^аеиоуыэюя]"
+
+    _re_rv = re.compile(_vowel)
+    _re_r1 = re.compile(_vowel + _non_vowel)
+
+    _re_perfective_gerund = re.compile(
+        r"(((?P<ignore>[ая])(в|вши|вшись))|(ив|ивши|ившись|ыв|ывши|ывшись))$"
+    )
+    _re_adjective = re.compile(
+        r"(ее|ие|ые|ое|ими|ыми|ей|ий|ый|ой|ем|им|ым|ом|его|ого|ему|ому|их|ых|"
+        r"ую|юю|ая|яя|ою|ею)$"
+    )
+    _re_participle = re.compile(
+        r"(((?P<ignore>[ая])(ем|нн|вш|ющ|щ))|(ивш|ывш|ующ))$"
+    )
+    _re_reflexive = re.compile(
+        r"(ся|сь)$"
+    )
+    _re_verb = re.compile(
+        r"(((?P<ignore>[ая])(ла|на|ете|йте|ли|й|л|ем|н|ло|но|ет|ют|ны|ть|ешь|"
+        r"нно))|(ила|ыла|ена|ейте|уйте|ите|или|ыли|ей|уй|ил|ыл|им|ым|ен|ило|"
+        r"ыло|ено|ят|ует|уют|ит|ыт|ены|ить|ыть|ишь|ую|ю))$"
+    )
+    _re_noun = re.compile(
+        r"(а|ев|ов|ие|ье|е|иями|ями|ами|еи|ии|и|ией|ей|ой|ий|й|иям|ям|ием|ем|"
+        r"ам|ом|о|у|ах|иях|ях|ы|ь|ию|ью|ю|ия|ья|я)$"
+    )
+    _re_superlative = re.compile(
+        r"(ейш|ейше)$"
+    )
+    _re_derivational = re.compile(
+        r"(ост|ость)$"
+    )
+    _re_i = re.compile(
+        r"и$"
+    )
+    _re_nn = re.compile(
+        r"((?<=н)н)$"
+    )
+    _re_ = re.compile(
+        r"ь$"
+    )
+
+    def stem(self, word):
+        rv_pos, r2_pos = self._find_rv(word), self._find_r2(word)
+        word = self._step_1(word, rv_pos)
+        word = self._step_2(word, rv_pos)
+        word = self._step_3(word, r2_pos)
+        word = self._step_4(word, rv_pos)
+        return word
+
+    def _find_rv(self, word):
+        rv_match = self._re_rv.search(word)
+        if not rv_match:
+            return len(word)
+        return rv_match.end()
+
+    def _find_r2(self, word):
+        r1_match = self._re_r1.search(word)
+        if not r1_match:
+            return len(word)
+        r2_match = self._re_r1.search(word, r1_match.end())
+        if not r2_match:
+            return len(word)
+        return r2_match.end()
+
+    def _cut(self, word, ending, pos):
+        match = ending.search(word, pos)
+        if match:
+            try:
+                ignore = match.group("ignore") or ""
+            except IndexError:
+                return True, word[:match.start()]
+            else:
+                return True, word[:match.start() + len(ignore)]
+        else:
+            return False, word
+
+    def _step_1(self, word, rv_pos):
+        match, word = self._cut(word, self._re_perfective_gerund, rv_pos)
+        if match:
+            return word
+        _, word = self._cut(word, self._re_reflexive, rv_pos)
+        match, word = self._cut(word, self._re_adjective, rv_pos)
+        if match:
+            _, word = self._cut(word, self._re_participle, rv_pos)
+            return word
+        match, word = self._cut(word, self._re_verb, rv_pos)
+        if match:
+            return word
+        _, word = self._cut(word, self._re_noun, rv_pos)
+        return word
+
+    def _step_2(self, word, rv_pos):
+        _, word = self._cut(word, self._re_i, rv_pos)
+        return word
+
+    def _step_3(self, word, r2_pos):
+        _, word = self._cut(word, self._re_derivational, r2_pos)
+        return word
+
+    def _step_4(self, word, rv_pos):
+        _, word = self._cut(word, self._re_superlative, rv_pos)
+        match, word = self._cut(word, self._re_nn, rv_pos)
+        if not match:
+            _, word = self._cut(word, self._re_, rv_pos)
+        return word
+
+
+def diff_count(iter1, iter2):
+    count = abs(len(iter1) - len(iter2))
+    for e1, e2 in zip(iter1, iter2):
+        if e1 != e2:
+            count += 1
+    return count
+
+
+def find0(word):
+    list_words = [word.strip() for word in open("dict.txt", "rt").readlines()]
+    stemmer = Stemmer()
+    base_word = stemmer.stem(word)
+    source_word = word
+    print(f"Base: {base_word}")
+    print("Список подходящих слов:")
+    for word in list_words:
+        if word.find(base_word) == 0 and source_word != word:
+            print(word)
+
+
+def find1(word):
+    list_words = [word.strip() for word in open("dict.txt", "rt").readlines()]
+    source_word = word
+    print("Подходящие слова(2 различия):")
+    for word in list_words:
+        if diff_count(word, source_word) < 3 and source_word != word:
+            print(word)
